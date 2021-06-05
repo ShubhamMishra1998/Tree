2e7c779fd727c2eabae2c4a85b3e538faf477f49
Shubham Mishra <sm544735@gmail.com>
2021-06-05 14:12:05 +0530

    Create How to scan string with spaces.cpp

---
+++ How to scan string with spaces.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        char str[400];
+        //scanf(" %[^\n]s",str);
+        scanf(" %[^\n]s",str);
+        cout<<str<<endl;
+        
+    }
+}
